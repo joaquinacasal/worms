68f604bf1927e9327542fd1c8368fdfa1e2b9857
Joaquin Casal <joaquinarielcasal@gmail.com>
2018-06-11 23:48:32 -0300

    Ventana y captura de eventos SDL

--- src/client/client_Main.cpp
+++ src/client/client_Main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <SDL2/SDL.h>
+#include "client_SdlWindow.h"
 #include "../common/common_SocketProtocol.h"
 #include "../common/common_SafeQueue.h"
 #include "../common/common_BlockingQueue.h"
@@ -7,11 +9,7 @@
 #include "client_CapturedEventFactory.h"
 #include "client_CapturedEventSender.h"
 
-bool is_valid_coordinate(int x, int y){
-  if (x < 0 || y < 0) return false;
-  return true;
-}
-
+enum InputState { WAITING_COMMAND, WAITING_RADIO_CLICK, WAITING_TELE_CLICK };
 
 int main(int argc, char* argv[]){
   SocketProtocol sp("127.0.0.1", argv[1]);
@@ -27,44 +25,79 @@ int main(int argc, char* argv[]){
   captured_event_sender.start();
   CapturedEventFactory captured_event_factory(sp, blocking_queue);
 
+  SdlWindow window(800, 600); //TODO: Maximized
+  window.fill();
+  InputState state = WAITING_COMMAND;
   int x, y;
   while (true){
-      std::cout << "Ingresá un comando: ";
-      std::string input;
-      std::cin >> input;
       if (!drawable_factory.is_connected()) break;
-      if (input.length() != 1) continue;
-      if (input.at(0) == 'q') {
-        captured_event_factory.create_closed_connection_event();
-        break;
-      }
-      switch (input.at(0)) {
-        case 'd': captured_event_factory.create_move_right_event(); break; // Right arrow
-        case 'a': captured_event_factory.create_move_left_event(); break;  // Left arrow
-        case 's': captured_event_factory.create_stop_moving_event(); break; // Down arrow
-        case 'w': captured_event_factory.create_jump_forward_event(); break;       // Enter
-        case 'r': captured_event_factory.create_jump_backward_event(); break;   // Backspace
-        case '7': captured_event_factory.create_dynamite_event(); break; // Dynamite
-        case '8': // Radiocontrolled
-          std::cout << "Ingresá la coordenada:\nX: ";
-          std::cin >> x;
-          std::cout << "Y: ";
-          std::cin >> y;
-          if (is_valid_coordinate(x, y))
+      SDL_Event event;
+      window.fill();
+      SDL_WaitEvent(&event);
+      switch (event.type) {
+        case SDL_KEYDOWN: {
+          SDL_KeyboardEvent& keyEvent = (SDL_KeyboardEvent&) event;
+          switch (keyEvent.keysym.sym) {
+            case SDLK_RIGHT:
+              captured_event_factory.create_move_right_event();
+              state = WAITING_COMMAND;
+              std::cout << "Flecha derecha" << std::endl;
+              break;
+            case SDLK_LEFT:
+              captured_event_factory.create_move_left_event();
+              state = WAITING_COMMAND;
+              std::cout << "Flecha izquierda" << std::endl;
+              break;
+            case SDLK_DOWN:
+              captured_event_factory.create_stop_moving_event();
+              state = WAITING_COMMAND;
+              std::cout << "Flecha abajo" << std::endl;
+              break;
+            case SDLK_UP:
+              captured_event_factory.create_jump_forward_event();
+              state = WAITING_COMMAND;
+              std::cout << "Flecha arriba" << std::endl;
+              break;
+            case SDLK_BACKSPACE:
+              captured_event_factory.create_jump_backward_event();
+              state = WAITING_COMMAND;
+              std::cout << "Barra espaciadora" << std::endl;
+              break;
+            case SDLK_d:
+              captured_event_factory.create_dynamite_event();
+              state = WAITING_COMMAND;
+              std::cout << "Letra D" << std::endl;
+              break;
+            case SDLK_r:
+              state = WAITING_RADIO_CLICK;
+              std::cout << "Letra R" << std::endl;
+              break;
+            case SDLK_t:
+              state = WAITING_TELE_CLICK;
+              std::cout << "Letra T" << std::endl;
+              break;
+            case SDLK_q: 
+              captured_event_factory.create_closed_connection_event();
+              break;
+          }
+        }
+        case SDL_MOUSEBUTTONDOWN:
+          if (event.button.button != SDL_BUTTON_LEFT ||
+          (state != WAITING_RADIO_CLICK && state != WAITING_TELE_CLICK)){
+            break;
+          }
+          SDL_GetMouseState(&x, &y);
+          if (state == WAITING_RADIO_CLICK){
             captured_event_factory.create_radiocontrolled_event(x, y);
-          break;
-        case '9': // Teletransportation
-          std::cout << "Ingresá la coordenada:\nX: ";
-          std::cin >> x;
-          std::cout << "Y: ";
-          std::cin >> y;
-          if (is_valid_coordinate(x, y))
+          } else {
             captured_event_factory.create_teletransportation_event(x, y);
-          break;
-        default:
-          std::cout << "Comando no reconocido\n";
+          }
+          state = WAITING_COMMAND;
+          std::cout << "X: " << x << std::endl;
+          std::cout << "Y: " << y << std::endl;
           break;
       }
+      window.render();
   }
   drawable_factory.join();
   console_drawer.stop();
